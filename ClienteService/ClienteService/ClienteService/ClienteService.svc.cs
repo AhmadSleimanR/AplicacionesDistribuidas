86bdd14b33b6d1089a424070929279fef0494718
AhmadSleimanR <werty_51@hotmail.com>
2019-08-26 00:10:41 -0500

    Added ClienteService
    Added the service for clientes

---
+++ ClienteService/ClienteService/ClienteService/ClienteService.svc.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ClienteService
+{
+   public class ClienteService : IClienteService
+    {
+
+        public String Insert(Cliente cliente)
+        {
+            try
+            {
+                String msg;
+                SqlConnection con = new SqlConnection("Server=.;Database=RestauranteBD;User Id=sa;Password = 12345;");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("insert into CLIENTES(username, userpass, nombre, apellidos, email, direccion, dni)" +
+                    "values(@Username,@Userpass,@Nombre,@Apellidos,@Email,@Direccion,@Dni)", con);
+                cmd.Parameters.AddWithValue("@Username", cliente.Username);
+                cmd.Parameters.AddWithValue("@Userpass", cliente.Userpass);
+                cmd.Parameters.AddWithValue("@Nombre", cliente.Nombre);
+                cmd.Parameters.AddWithValue("@Apellidos", cliente.Apellidos);
+                cmd.Parameters.AddWithValue("@Email", cliente.Email);
+                cmd.Parameters.AddWithValue("@Direccion", cliente.Direccion);
+                cmd.Parameters.AddWithValue("@Dni", cliente.Dni);
+                int operation = cmd.ExecuteNonQuery();
+                con.Close();
+                if (operation == 1) {
+                    msg = "Operacion realizada con exito!";
+                }
+                else
+                {
+                    msg = "La operacion no se pudo realizar.";
+                }
+                return msg;
+            }
+            catch (Exception e) {
+                Console.WriteLine("Error: "+e);
+                return "Error"+e;
+            }
+        }
+
+        public Cliente FindCliente(int clientes_id)
+        {
+            Cliente cliente = new Cliente();
+            try
+            {
+                SqlConnection con = new SqlConnection("Server=.;Database=RestauranteBD;User Id=sa;Password = 12345;");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from CLIENTES where clientes_id=@Clientes_id", con);
+                cmd.Parameters.AddWithValue("@Clientes_id", clientes_id);
+                SqlDataReader dtr = cmd.ExecuteReader();
+                dtr.Read();
+                cliente.Clientes_id = int.Parse(dtr["clientes_id"].ToString());
+                cliente.Username = dtr["username"].ToString();
+                cliente.Userpass = dtr["userpass"].ToString();
+                cliente.Nombre = dtr["nombre"].ToString();
+                cliente.Apellidos = dtr["apellidos"].ToString();
+                cliente.Email = dtr["email"].ToString();
+                cliente.Direccion = dtr["direccion"].ToString();
+                cliente.Dni = dtr["dni"].ToString();
+                con.Close();
+                return cliente;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e);
+                return null;
+            }
+        }
+
+        public string Update(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Delete(int cliente_id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ClienteService/ClienteService/ClienteService/IClienteService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ClienteService
+{
+    [ServiceContract]
+    public interface IClienteService
+    {
+        [OperationContract]
+        String Insert(Cliente cliente);
+        [OperationContract]
+        String Update(Cliente cliente);
+        [OperationContract]
+        Cliente FindCliente(int cliente_id);
+        [OperationContract]
+        String Delete(int cliente_id);
+    }
+
+    [DataContract]
+    public class Cliente {
+        int clientes_id;
+        string username;
+        string userpass;
+        string nombre;
+        string apellidos;
+        string email;
+        string direccion;
+        string dni;
+
+        [DataMember]
+        public int Clientes_id
+        {
+            get { return clientes_id; }
+            set { clientes_id = value; }
+        }
+        [DataMember]
+        public String Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        [DataMember]
+        public String Userpass
+        {
+            get { return userpass; }
+            set { userpass = value; }
+        }
+        [DataMember]
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        [DataMember]
+        public String Apellidos
+        {
+            get { return apellidos; }
+            set { apellidos = value; }
+        }
+        [DataMember]
+        public String Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        [DataMember]
+        public String Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+        [DataMember]
+        public String Dni
+        {
+            get { return dni; }
+            set { dni = value; }
+        }
+    }
+
+}
