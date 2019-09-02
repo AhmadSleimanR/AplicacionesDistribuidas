ab40bb59f23aa4f06d761f4ba12e36d3b340b15a
AnibalMA <anibal.marlo@intelecta.biz>
2019-09-01 22:57:29 -0500

    Update

---
+++ MenuService/TestServiceWCF/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestServiceWCF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("TestServiceWCF")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9d27da88-373e-419d-9b82-911db10eeb15")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MenuService/TestServiceWCF/UnitTest1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ServiceModel;
+namespace TestServiceWCF
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCrear()
+        {
+            PlatosServ.PlatosClient platosClient = new PlatosServ.PlatosClient();
+
+            PlatosServ.Plato plato= platosClient.RegistrarPlato(
+                new PlatosServ.Plato
+                {
+                    Id_plato = 9999,
+                    Descripcion = "Arroz  Chaufa",
+                    Precio = (decimal)17.50,
+                    Url = "https://developersonline2019.net/resources/21.png"
+                }
+            );
+
+            Assert.AreEqual(9999, plato.Id_plato);
+            Assert.AreEqual("Arroz  Chaufa", plato.Descripcion);
+            Assert.AreEqual(17.50, plato.Precio);
+            Assert.AreEqual("https://developersonline2019.net/resources/21.png", plato.Url);
+
+        }
+        [TestMethod]
+        public void TestModificar()
+        {
+            PlatosServ.PlatosClient platosClient = new PlatosServ.PlatosClient();
+
+            PlatosServ.Plato plato = platosClient.RegistrarPlato(
+                new PlatosServ.Plato
+                {
+                    Id_plato = 9999,
+                    Descripcion = "Arroz Blanco",
+                    Precio = (decimal)17.50,
+                    Url = "https://developersonline2019.net/resources/21.png"
+                }
+            );
+
+            Assert.AreEqual(9999, plato.Id_plato);
+            Assert.AreEqual("Arroz Blanco", plato.Descripcion);
+            Assert.AreEqual(20.50, plato.Precio);
+            Assert.AreEqual("https://developersonline2019.net/resources/21.png", plato.Url);
+
+        }
+        [TestMethod]
+        public void TestRegistroRepetido()
+        {
+            PlatosServ.PlatosClient platosClient = new PlatosServ.PlatosClient();
+            try
+            {
+                PlatosServ.Plato plato = platosClient.RegistrarPlato(
+                    new PlatosServ.Plato
+                    {
+                        Descripcion = "Arroz Chaufa",
+                        Precio = (decimal)17.50,
+                        Url = "https://developersonline2019.net/resources/21.png"
+                    }
+                );
+            }
+            catch (FaultException<PlatosServ.RepetidoException> err)
+            {
+                Assert.AreEqual("Error al ingresar el registro", err.Reason.ToString());
+                Assert.AreEqual(err.Detail.codigo, "101");
+                Assert.AreEqual(err.Detail.descripcion, "El plato a registrar, ya existe.");
+                throw;
+            }
+            
+        }
+        [TestMethod]
+        public void TestEliminarRegistro()
+        {
+            PlatosServ.PlatosClient platosClient = new PlatosServ.PlatosClient();
+
+            platosClient.EliminarPlato(9999);
+        }
+        [TestMethod]
+        public void TestListar()
+        {
+            PlatosServ.PlatosClient platosClient = new PlatosServ.PlatosClient();
+
+            platosClient.ObtenerPlatos();
+        }
+        [TestMethod]
+        public void TestBusquedaNoEncontrada()
+        {
+            PlatosServ.PlatosClient platosClient = new PlatosServ.PlatosClient();
+            platosClient.ObtenerPlatobyName("Causa Rellena");
+        }
+        
+
+    }
+}
