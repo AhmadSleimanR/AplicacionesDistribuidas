e8db2813391fa290e1fc197e3130fa0985f28cd5
AhmadSleimanR <werty_51@hotmail.com>
2019-09-12 14:26:02 -0500

    Updated UNIT TEST
    Updated the UNIT TEST client

--- ClienteService/ClienteUnitTest/ClienteUnitTest/UnitTest1.cs
+++ ClienteService/ClienteUnitTest/ClienteUnitTest/UnitTest1.cs
@@ -30,19 +30,41 @@ namespace ClienteUnitTest
         [TestMethod]
         public void UpdateClientes()
         {
-
+            string jsonString = @"{
+                                 ""id"":""1"",
+                                 ""username"":""UPDATE"",
+                                 ""userpass"":""admin123"",
+                                 ""nombre"":""testnombre"",
+                                 ""apellidos"":""unit test"",
+                                 ""email"":""werty_51@hotmail.com"",
+                                 ""direccion"":""test direccion"",
+                                 ""dni"":""75116591""
+                                }";
+            RestAPIHelper<CreatedCliente> restAPI = new RestAPIHelper<CreatedCliente>();
+            var restURL = restAPI.SetUrl("/api/clientes/1");
+            var restRequest = restAPI.CreatePutRequest(jsonString);
+            var response = restAPI.GetResponse(restURL, restRequest);
+            CreatedCliente content = restAPI.GetContent<CreatedCliente>(response);
+            Assert.AreEqual("UPDATE", content.username);
         }
 
         [TestMethod]
         public void DeleteCliente()
         {
-
+            RestAPIHelper<CreatedCliente> restAPI = new RestAPIHelper<CreatedCliente>();
+            var restURL = restAPI.SetUrl("/api/clientes/8");
+            var restRequest = restAPI.CreateDeleteRequest();
         }
 
         [TestMethod]
         public void FindCliente()
         {
-
+            RestAPIHelper<CreatedCliente> restAPI = new RestAPIHelper<CreatedCliente>();
+            var restURL = restAPI.SetUrl("/api/clientes/8");
+            var restRequest = restAPI.CreateGetRequest();
+            var response = restAPI.GetResponse(restURL, restRequest);
+            CreatedCliente content = restAPI.GetContent<CreatedCliente>(response);
+            Assert.AreEqual("Test", content.nombre);
         }
     }
 }
